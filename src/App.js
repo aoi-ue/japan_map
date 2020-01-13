0d7bc9a8e2520b044a6d8a20b76cbe679c2d1e63
yuhanyuhan <yuhanlian@gmail.com>
2020-01-13 14:51:51 +0800

    Loading main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ReactTooltip from "react-tooltip";
+
+import JapanMap from './JapanMap'; 
+import Table from './Table'; 
+
 
 function App() {
+  const [content, setContent] = useState("");
+  console.log(content)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JapanMap setTooltipContent={setContent}/ >
+      <ReactTooltip>{content}</ReactTooltip>
+      <Table content={content}/>
     </div>
   );
 }
